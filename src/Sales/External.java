7b749115b1ac7e62ff0f5355ba617fd80751fdc0
Eduardo Costa <eduardocosta942z@gmail.com>
2025-06-10 19:06:46 -0300

    Created class External inheriting Seller.java

---
+++ src/Sales/External.java
@@ -0,0 +1,44 @@
+package Sales;
+
+public class External extends Seller {
+    // Declaring attributes:
+    private double traveledDistance;
+    private int sellsAmount;
+
+    // Special Methods:
+        // Builder:
+    public External(String cpf, String name, double salesValues, double baseSalary) {
+        super(cpf, name, salesValues, baseSalary);
+    }
+
+        // Getters:
+    public double getTraveledDistance() {
+        return traveledDistance;
+    }
+
+    public int getSellsAmount() {
+        return sellsAmount;
+    }
+
+        // Setters:
+    public void setTraveledDistance(double traveledDistance) {
+        this.traveledDistance = traveledDistance;
+    }
+
+    public void setSellsAmount(int sellsAmount) {
+        this.sellsAmount = sellsAmount;
+    }
+
+        // toString:
+    public String toString() {
+        return super.toString() +
+                ", traveledDistance=" + traveledDistance
+                + ", sellsAmount=" + sellsAmount;
+    }
+
+    // Behaviour Methods:
+    @Override
+    public double calcSalary() {
+        return super.getBaseSalary() + super.getSalesValues()/sellsAmount + 0.80 * traveledDistance;
+    }
+}
